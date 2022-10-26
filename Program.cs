6db596b2ca741a3a1ff5e6a9fe904f3c5b28b916
Dynatrace <minkook.kang@dynatrace>
2022-10-27 08:00:41 +1100

    microsoft data sqlclient

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Text;
 using System.Net;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace HttpListenerExample
 {
